d923ab59548dca5681898d60f2126f2adb93353d
abeMedia <info@abemedia.co.uk>
2014-02-26 07:26:01 +0100

    Update

--- js/stuff.js
+++ js/stuff.js
@@ -5,3 +5,27 @@ $.getScript('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.2/js/bootstr
 		container: "body"
 	});
 });
+
+function selectCity() {
+	var needle = $('#city').val(),
+		tmpl = '<div class="media"><div class="datebox"><small>{month}</small>{day}</div><div class="media-body">{city}</div></div>',
+		month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		output = [];
+		
+	for (i=0;i<schedule.length;i++) {
+		if(schedule[i].city == needle) {
+			data = schedule[i];
+			date = new Date(data.date.split('/')[2], data.date.split('/')[1] - 1, data.date.split('/')[0]);
+			data.day = (date.getDate()<=9 ? '0' + date.getDate() : date.getDate());
+			data.month = month_names[date.getMonth()];
+			
+			html = tmpl;
+			for (field in data) {
+				html = html.replace(RegExp("\\{" + field + "\\}", "gi"), data[field]);
+			}
+			output.push(html);
+	console.log(html);
+		}
+	}
+	$('#collectionDates').html(output.join(''));
+}
